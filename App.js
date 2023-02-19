a7fc301200ac9b4c16ad17a600e77d39e141cc44
YevheniiaVier <gotashk1983@gmail.com>
2023-02-19 23:39:56 +0200

    adds fonts

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import {
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Keyboard,
+  Alert,
+  // Button,
+} from "react-native";
+import { styles } from "./styles";
+
+const RobotoRegular = require("./assets/fonts/Roboto/Roboto-Regular.ttf");
+const RobotoBold = require("./assets/fonts/Roboto/Roboto-Bold.ttf");
+
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+//     "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+//   });
+// };
 
 export default function App() {
+  // const [isReady, setIsReady] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": RobotoRegular,
+    "Roboto-Bold": RobotoBold,
+  });
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+  if (!fontsLoaded) {
+    return null;
+    // (
+    //   <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
+    // );
+  }
   return (
-    <View style={styles.container}>
-      <Text>My React Native Project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            placeholder="Username"
+            value={name}
+            onChangeText={nameHandler}
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={passwordHandler}
+            style={styles.input}
+          />
+          {/* <Text>My React Native Project</Text> */}
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.btn}
+            onPress={onLogin}
+          >
+            <Text style={styles.btnTitle}>Login</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+
+        {/* <StatusBar style="auto" /> */}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
---
+++ styles.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Platform } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: Platform.OS === "ios" ? 50 : 100,
+    flex: 1,
+    // padding: 24,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "62dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+  btn: {
+    backgroundColor: "#ffb6c1",
+    height: 40,
+    borderRadius: 6,
+    marginTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTitle: {
+    color: "#fff",
+  },
+});
