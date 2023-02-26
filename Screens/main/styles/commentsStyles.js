ac826b640264b04ae7439ee00b1bc0f956237601
YevheniiaVier <gotashk1983@gmail.com>
2023-02-27 01:09:40 +0200

    adds basic comments page

---
+++ Screens/main/styles/commentsStyles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  goBackBtn: {
+    position: "absolute",
+    left: 16,
+    top: -34,
+    zIndex: 1,
+  },
+  imgWrap: {
+    width: "100%",
+    height: 240,
+  },
+  img: {
+    width: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    borderRadius: 16,
+  },
+  commentsListWrap: {
+    flex: 1,
+    marginTop: 32,
+  },
+  commentsInputWrap: {
+    marginBottom: 16,
+    marginTop: 16,
+    position: "relative",
+  },
+  commentsInput: {
+    height: 50,
+    paddingLeft: 16,
+    paddingRight: 42,
+    backgroundColor: "#E8E8E8",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 25,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+  },
+  commentsSubmitBtn: {
+    position: "absolute",
+    top: "50%",
+    transform: [{ translateY: -17 }],
+    right: 8,
+    width: 34,
+    height: 34,
+    backgroundColor: "#FF6C00",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 17,
+  },
+});
--- Screens/nestedScreens/CommentsScreen.js
+++ Screens/nestedScreens/CommentsScreen.js
@@ -1,10 +1,44 @@
 const { View, Text } = require("react-native");
-import { styles } from "../main/styles/mainStyles";
+import { styles } from "../main/styles/commentsStyles";
+
+import {
+  StyleSheet,
+  View,
+  Image,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+} from "react-native";
+
+import { AntDesign } from "@expo/vector-icons";
 
 export const CommentsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>CommentsScreen</Text>
+      <View style={styles.imgWrap}>
+        <Image source={{ uri: img }} style={styles.img} />
+      </View>
+      <View style={styles.commentsListWrap}>
+        <FlatList />
+      </View>
+      <View style={styles.commentsInputWrap}>
+        <TextInput
+          placeholder="Comment..."
+          placeholderTextColor={"#BDBDBD"}
+          value={comment}
+          // onChange={() => {
+          // }}
+          style={styles.commentsInput}
+        />
+        <TouchableOpacity
+          style={styles.commentsSubmitBtn}
+          // onPress={onSubmit}
+        >
+          <AntDesign name="arrowup" size={14} color="#ffffff" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
